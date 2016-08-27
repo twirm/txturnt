a72e23aa18783214337a17d6ee703bde35ea19ca
lukewhyte <lukeawhyte@gmail.com>
2016-08-27 15:44:54 -0500

    Add basic form markup and validation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,43 @@
-$(document).foundation();
+$(document).ready(function () {
+  $(document).foundation();
+
+  /*
+   * form validation
+   */
+  function Validator () {
+    var validator = this;
+    this.fields = {
+      'FSTNAM': validator.defaultTest,
+      'LSTNAM': validator.defaultTest,
+      'MZIPCD': validator.zipTest,
+    },
+    this.validate = this.validate.bind(this);
+    this.init();
+  }
+
+  Validator.prototype = {
+    defaultTest: function (str) {
+      return /\S/.test(str);
+    },
+
+    zipTest: function (str) {
+      return str === '' || /^\d{5}$/.test(str);
+    },
+
+    validate: function (func, key, el) {
+      var display = func($(el).val()) ? 'hide' : 'show';
+      $('.form-error#' + key)[display]();
+    },
+
+    init: function () {
+      var validator = this;
+      _.forEach(this.fields, function (func, key) {
+        $('input[name="' + key + '"]').blur(function (e) {
+          validator.validate(func, key, e.target);
+        })
+      });
+    },
+  };
+
+  new Validator();
+});
